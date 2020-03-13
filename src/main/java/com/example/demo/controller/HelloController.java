b6b227c1adc9fc3859d393553868d8384d068d46
santosh kumar <santosh143572@gmail.com>
2020-03-13 15:01:25 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+
+    public @ResponseBody
+    String hello() {
+        return "hello world";
+    }
+}
